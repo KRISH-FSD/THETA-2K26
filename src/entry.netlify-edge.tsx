538947ef99324bfa51b6b98ae5eca558953e1faa
krishjerry31 <krishpersonal05@gmail.com>
2026-04-04 09:21:05 +0530

    commited before deployement

--- src/entry.netlify-edge.tsx
+++ src/entry.netlify-edge.tsx
@@ -1,12 +1,2 @@
-import {
-  createQwikCity,
-  type PlatformNetlify,
-} from "@builder.io/qwik-city/middleware/netlify-edge";
-import qwikCityPlan from "@qwik-city-plan";
-import render from "./entry.ssr";
-
-declare global {
-  type QwikCityPlatform = PlatformNetlify;
-}
-
-export default createQwikCity({ render, qwikCityPlan });
+// Deprecated: Migrated to Vercel
+// Please delete this file.
